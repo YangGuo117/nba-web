63f114e9d67cf9a25fca99ddb9b37a81a1de688e
YangGuo117 <gy1045226947@gmail.com>
2019-11-18 23:48:30 -0600

    Updates

---
+++ precache-manifest.84983ca43c23d843e12a6dd7e5e4dd1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb2e10357bc5850008a9cf3c8a356cf",
+    "url": "/nba-web/index.html"
+  },
+  {
+    "revision": "8c42d1d998d3005cf21c",
+    "url": "/nba-web/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "fe8b2cf7fd4fb02b9d57",
+    "url": "/nba-web/static/css/main.8f4b5269.chunk.css"
+  },
+  {
+    "revision": "8c42d1d998d3005cf21c",
+    "url": "/nba-web/static/js/2.2e69e78e.chunk.js"
+  },
+  {
+    "revision": "fe8b2cf7fd4fb02b9d57",
+    "url": "/nba-web/static/js/main.5d8c3cc3.chunk.js"
+  },
+  {
+    "revision": "b925953658c1989ccc50",
+    "url": "/nba-web/static/js/runtime-main.f5136b55.js"
+  },
+  {
+    "revision": "9470e2d5c6c7f425edc8e9d8baebdb05",
+    "url": "/nba-web/static/media/nba-logoman-word-white.9470e2d5.svg"
+  }
+]);
